fd66a4bc8693a51f2ee44f56491e6dbaead007af
Arthefacte <62467445+Arthefacte@users.noreply.github.com>
2026-07-02 16:18:04 +0200

    feat(i18n): clés buyback 4 pools (LB_TAB_BUYBACK/BB_POOL_LABEL/RES_BUYBACK)

---
+++ test/buyback-i18n.test.js
@@ -0,0 +1,26 @@
+const test = require("node:test");
+const assert = require("node:assert");
+globalThis.window = {};
+require("../i18n.js");
+const T = window.FA_I18N.T;
+
+test("nouvelles clés présentes en FR/EN/ZH", () => {
+  for (const k of ["LB_TAB_BUYBACK", "BB_POOL_LABEL", "RES_BUYBACK"]) {
+    assert.ok(T[k], `${k} manquante`);
+    for (const lang of ["FR", "EN", "ZH"]) assert.ok(T[k][lang], `${k}.${lang} manquante`);
+  }
+});
+
+test("clés retirées absentes", () => {
+  for (const k of ["LB_TAB_BURNED", "LB_TAB_LIQUIDITY", "LB_TAB_AIRDROP", "RES_POOL", "RES_BURN", "BB_LIQ", "BB_BOUGHT"]) {
+    assert.ok(!T[k], `${k} devrait être supprimée`);
+  }
+});
+
+test("BB_POOL_LABEL contient un placeholder %s", () => {
+  assert.ok(/%s/.test(T.BB_POOL_LABEL.FR));
+});
+
+test("FG_SUB ne mentionne plus l'ancien split 70/30", () => {
+  assert.ok(!/70%|Reward Pool|Mega buyback/.test(T.FG_SUB.FR));
+});
