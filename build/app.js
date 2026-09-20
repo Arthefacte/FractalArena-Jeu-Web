850f7e2869c26581cb53f97223d713671f9f6147
Arthefacte <62467445+Arthefacte@users.noreply.github.com>
2026-09-20 22:26:27 +0200

    chore(web): medaillons 3D 28px -> 40px

--- build/app.js
+++ build/app.js
@@ -5121,12 +5121,12 @@ function Header({
     key: "lq" + liquidPop.n,
     className: cx("chip", "liquid", liquidPop.n > 0 && "pop")
   }, /*#__PURE__*/React.createElement(Jeton3D, {
-    px: 28,
+    px: 40,
     fallback: /*#__PURE__*/React.createElement("img", {
       src: "assets/TOKEN.png",
       alt: "",
-      width: "28",
-      height: "28",
+      width: "40",
+      height: "40",
       style: {
         display: "block"
       }
@@ -5139,7 +5139,7 @@ function Header({
   }, /*#__PURE__*/React.createElement("b", {
     className: "chip-amount"
   }, fbFmt(fbBal.fb_earned_sats)), /*#__PURE__*/React.createElement(Jeton3D, {
-    px: 28,
+    px: 40,
     fallback: /*#__PURE__*/React.createElement("span", {
       className: "chip-lbl"
     }, "FB")
@@ -5152,12 +5152,12 @@ function Header({
     key: "lk" + lockedPop.n,
     className: cx("chip", "locked", lockedPop.n > 0 && "pop")
   }, /*#__PURE__*/React.createElement(Jeton3D, {
-    px: 28,
+    px: 40,
     fallback: /*#__PURE__*/React.createElement("img", {
       src: "assets/TOKEN.png",
       alt: "",
-      width: "28",
-      height: "28",
+      width: "40",
+      height: "40",
       style: {
         display: "block"
       }
